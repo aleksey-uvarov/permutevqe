517d8b08f5521ad72e9d8cb3de5936601e94f756
Alexey Uvarov <34627976+aleksey-uvarov@users.noreply.github.com>
2022-06-14 14:03:40 +0300

    Add plotting function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from qiskit.algorithms import VQE
 from qiskit.utils import QuantumInstance
 from qiskit import Aer
@@ -13,6 +14,7 @@ from functools import reduce
 from itertools import permutations
 from math import factorial
 import time
+from scipy.special import lambertw
 
 
 def circular_chiral_walk(n_sites: int, alpha: float):
@@ -32,15 +34,15 @@ def circular_chiral_walk(n_sites: int, alpha: float):
 def all_permutations_experiment() -> None:
     # Problem Hamiltonian
     n_qubits = 3
-    h = circular_chiral_walk(n_qubits, 0.1)
+    h = circular_chiral_walk(n_qubits, np.pi / 2)
 
     # Error model parameters
     depol_error_rates = [1e-3, 1e-2, 2e-2]
 
-    samples_per_permutation = 1
+    samples_per_permutation = 10
 
     # Ansatz parameters
-    spsa = SPSA(maxiter=100)
+    spsa = SPSA(maxiter=1000)
     ansatz = TwoLocal(rotation_blocks=['ry'],
                       entanglement_blocks='cz',
                       reps=2,
@@ -68,42 +70,34 @@ def all_permutations_experiment() -> None:
             result = vqe.compute_minimum_eigenvalue(operator=h)
             results[i, j] = result.eigenvalue
             print(result.eigenvalue)
+            print(result.optimizer_time)
     print(results)
-    np.savetxt("permutation_vqe" + str(int(time.time())) + ".txt", results)
+    np.savetxt("permutation_vqe_" + str(int(time.time())) + ".txt", results)
+
+
+def inverse_factorial(x: int) -> complex:
+    # mathoverflow.net/questions/12828
+    c = 0.036534
+    L = np.log((x + c) / (2 * np.pi)**0.5)
+    return L / lambertw(L / np.e) + 0.5
+
+
+def plot_permutations_experiment(expt_time: str):
+    data = np.loadtxt("permutation_vqe_" + expt_time + ".txt")
+    n_qubits = int(inverse_factorial(data.shape[0]).real)
+    print(data.shape)
+    for i in range(data.shape[1]):
+        plt.scatter(np.arange(data.shape[0]), data[:, i], alpha=0.3, color="tab:blue")
+    plt.boxplot(data.T, flierprops={"marker": ".",}, whis=(10, 90), positions=np.arange(data.shape[0]))
+    perms = list(permutations(range(n_qubits)))
+    print(perms)
+    plt.xticks(np.arange(data.shape[0]), perms)
+    plt.ylabel("VQE energy")
+    plt.savefig("permutations_" + expt_time + ".png", format='png', bbox_inches='tight', dpi=400)
+    plt.show()
 
 
 if __name__ == '__main__':
-    all_permutations_experiment()
-    # h = -1 * (Z ^ Z ^ I) - 1 * (I ^ Z ^ Z) - 1 * (X ^ X ^ X) # careful with the brackets!
-    # h = circular_chiral_walk(3, 0.1)
-    # # print(h)
-    # adam = ADAM(maxiter=100)  # uses jacobian but is slow for some reason
-    # bfgs = L_BFGS_B(maxfun=100)  # passes jacobian to scipy
-    # spsa = SPSA(maxiter=100)  # doesn't use partial derivative
-    # ansatz = TwoLocal(rotation_blocks=['ry'], entanglement_blocks='cz', reps=3, num_qubits=h.num_qubits)
-    #
-    # grad = Gradient(grad_method='param_shift')
-    #
-    # noise_model = NoiseModel()
-    # error_rates = [1e-3, 1e-2, 2e-2]
-    # for i, rate in enumerate(error_rates):
-    #     noise_model.add_quantum_error(depolarizing_error(rate, 1),
-    #                                   ['u1', 'u2', 'u3'], [i])
-    #
-    # print(noise_model)
-    # all_permutations_experiment(h, noise_model)
-
-    # quantum_instance = QuantumInstance(backend=Aer.get_backend("qasm_simulator"),
-    #                                    shots=1024,
-    #                                    noise_model=noise_model)
-    #
-    # vqe = VQE(ansatz,
-    #           optimizer=spsa,
-    #           quantum_instance=quantum_instance,
-    #           gradient=grad,
-    #           initial_point=np.random.randn(ansatz.num_parameters) * 1e-4)
-    #
-    # result = vqe.compute_minimum_eigenvalue(operator=h)
-    # print("spsa")
-    # print(result.eigenvalue)
-    # print(result.optimizer_time)
+    # all_permutations_experiment()
+    plot_permutations_experiment("1655203364")
+    # print(inverse_factorial(factorial(5)))
